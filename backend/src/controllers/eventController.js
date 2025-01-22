b72948b93090a93268fe4619c139d4d71b6cbbe9
adi673 <codingadityawadgaonkar@gmail.com>
2025-01-22 21:53:12 +0530

    little changes

--- backend/src/controllers/eventController.js
+++ backend/src/controllers/eventController.js
@@ -5,12 +5,16 @@ const User = require('../models/userModel');
 exports.createEvent = async (req, res) => {
     try {
         const { title, description, date, location, moderators } = req.body;
-
+        let status='pending';
         // Ensure only admins or moderators can create events
-        if (req.user.role !== 'admin' && req.user.role !== 'SuperAdmin') {
+        if (req.user.role !== 'Employee' && req.user.role !== 'Admin' && req.user.role !== 'SuperAdmin') {
             return res.status(403).json({ message: 'Access denied.' });
         }
 
+        if (req.user.role == 'SuperAdmin') {
+           status='approved';
+        }
+        
         // Validate moderators' roles
         const validModerators = await User.find({
             _id: { $in: moderators },
@@ -26,12 +30,37 @@ exports.createEvent = async (req, res) => {
             description,
             date,
             location,
+            status,
             createdBy: req.user.id,
             moderators,
         });
 
         await newEvent.save();
         res.status(201).json({ message: 'Event created successfully.', event: newEvent });
+    } catch (error) {
+        res.status(500).json({ message: 'Server error.', error });
+    }
+};
+
+
+exports.approveEvent = async (req, res) => {
+    try {
+        const { eventId } = req.params;
+
+        if (req.user.role !== 'SuperAdmin') {
+            return res.status(403).json({ message: 'Access denied.' });
+        }
+        const event = await Event.findByIdAndUpdate(
+            eventId,
+            { status: 'approved' },
+            { new: true }
+        );
+
+        if (!event) {
+            return res.status(404).json({ message: 'Event not found.' });
+        }
+
+        res.status(200).json({ message: 'Event approved.', event });
     } catch (error) {
         res.status(500).json({ message: 'Server error.', error });
     }
--- backend/src/routes/eventRoutes.js
+++ backend/src/routes/eventRoutes.js
@@ -4,10 +4,10 @@ const { register, login, logout } = require('../controllers/authController');
 const { validateRegister } = require('../middlewares/validationMiddleware');
 const { authorizeRoles } = require('../middlewares/roleAuthMiddleware')
 const { authMiddleware } = require('../middlewares/authMiddleware')
-const { createEvent }= require('../controllers/eventController')
+const { createEvent, approveEvent }= require('../controllers/eventController')
 //console.log("In authRoutes")
 router.post('/create', authMiddleware, authorizeRoles('Employee','Admin', 'SuperAdmin'), createEvent );
-router.post('/login' , login);
+router.put('/approve/:eventId', authMiddleware, authorizeRoles('SuperAdmin') , approveEvent);
 router.post('/logout', logout);
 
 module.exports = router;
